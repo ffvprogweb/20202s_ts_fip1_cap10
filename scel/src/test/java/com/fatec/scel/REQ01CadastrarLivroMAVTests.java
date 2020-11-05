cef790634301bcce041a748574393867dc4078b3
55119 <55119@192.168.100.6>
2020-11-05 15:02:29 -0200

    Cadastrar livro - mav test

---
+++ scel/src/test/java/com/fatec/scel/REQ01CadastrarLivroMAVTests.java
@@ -0,0 +1,16 @@
+package com.fatec.scel;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class REQ01CadastrarLivroMAVTests {
+
+	@Test
+	public void ct01_quando_seleciona_cadastrar_livro_retorna_http_200 () throws Exception{
+	//Dado – que o sistema está disponível
+	//Quando – o usuário faz uma solicitação do tipo GET - localhost:8080/livros/cadastrar
+	//Então – retorna o HTTP 200
+	}
+
+}
